ecad4ce498371da25c03180b55eb18a737043e36
Subhranil Mukherjee <subhranilmukherjee190@gmail.com>
2021-03-29 21:29:00 +0530

    updt: mitchell merritt: initial implementation of node and resources

--- assignment_06_mitchell_merritt.py
+++ assignment_06_mitchell_merritt.py
@@ -15,8 +15,8 @@ class Resource:
     def __init__(self, m, i):
         self.sem = m.Semaphore()
         self.id_ = i
-        self.acquired_by = m.Value('i', -1)
-
+        self.acquired_by = None
+        self.lock = m.Lock()
 
     def acquire(self, node):
         """
@@ -33,13 +33,20 @@ class Resource:
 
         """
         node.print("Trying to acquire R" + str(self.id_),
-                   "[Currently acquired by " + str(self.acquired_by.value) + "]")
+                   "[Currently acquired by " + str(self.acquired_by) + "]")
+        with self.lock:
+            if self.acquired_by == None:
+                self.acquired_by = node
+                self.sem.release()
+                return
+        self.acquired_by.grant(node)
+
+    def access(self, node):
         self.sem.acquire()
-        self.acquired_by.value = node.id_
         node.print("Acquired R" + str(self.id_))
+        self.acquired_by = node
 
-
-    def release(self, node):
+    def release(self):
         """
         Releases the resource object once served
 
@@ -53,10 +60,10 @@ class Resource:
         None.
 
         """
-        node.print("Released R" + str(self.id_))
-        self.sem.release()
-
-
+        with self.lock:
+            self.acquired_by.print("Released R" + str(self.id_))
+            self.acquired_by = None
+            self.sem.release()
 
 class Node:
 
@@ -65,9 +72,8 @@ class Node:
         self.id = id
         self.u = manager.Value('i',0)
         self.v = 0
-        self.u_lock = manager.Lock()
-        #self.blocked_by = manager.Value('i',-1)
         self.blocking = manager.List()
+        self.status_lock = manager.Lock()
 
     def fire(self, max_req, global_sema):
         """
@@ -109,22 +115,30 @@ class Node:
         num_res = random.randint(1, self.num_resource)  # number f resources required
         resources = random.sample(range(self.num_resource), num_res)  # which resources are required
         sleep_time = random.random() + random.random()
-        with self.res_tab_lock:
-            self.print("Resource list generated:", self.get_remaining_resources(resources))
-            for res in resources:
-                self.resource_table[res][self.id_] = -1  # update resource status table
-        i = 0  # number of resources held
+        self.print("Resource list generated:", self.get_remaining_resources(resources))
         for res in resources:
             self.resource_semaphores[res].acquire(self)
-            with self.res_tab_lock:
-                self.resource_table[res][self.id_] = 1
-            self.print("Remaining", self.get_remaining_resources(resources[i+1:]))
-            i += 1
+        for res in resources:
+            self.global_resource_list[res].access()
         # All resources accessed, now utilise
         self.execute(resources, sleep_time)
 
 
-    def grant(self,)
+    def print(self, *args):
+        """
+        Displays passed information along with id of the node
+
+        Parameters
+        ----------
+        *args : Pointer
+            Variable number of arguments.
+
+        Returns
+        -------
+        None.
+
+        """
+        print("[Node %3d] " % self.id_, *args)
 
     def execute(self, resources, sleep_time):
         """
@@ -143,38 +157,37 @@ class Node:
 
         """
         sleep(sleep_time)
-        for res in resources:
-            with self.res_tab_lock:
-                self.resource_table[res][self.id_] = 0
-            self.resource_semaphores[res].release(self)
+        for r in resources:
+            self.resource_list[r].release(self)
+        with self.status_lock:
+            self.blocking.clear()
 
-    def get_blocked(self, node, neighbors):
+    def grant(self, node):
+        with self.status_lock:
+            self.blocking.append(node)
+        node.transmit(self.u.value, True)
 
-        # current node gets blocked by argument 'node'
-        with self.u_lock:
-            self.u.value = max(self.u.value,node.u.value) + 1
-            self.v.value = self.u.value
-
-        self.blocked_by = node.id.value
-
-        self.transmit(neighbors)  # apply transmit rule after block rule
-
-    def transmit(self, neighbors):
-
-        # check all nodes to find which nodes are blocked by current node
-        for node in neighbors:
-            if node.blocked_by.value == self.id.value:
-                with node.u_lock:
-                    node.u.value = self.u.value
-                    node.transmit(neighbors)
-
-    def detect(self):
-
-        if self.blocked_by != -1:
-            with self.u_lock:
-                if self.u.value == self.v.value and self.u.value == self.blocked_by.u.value:
-                    # Deadlock
-                    pass
+    def transmit(self, val, initial=False):
+        with self.status_lock:
+            if not initial:
+                # if this is not the initial transmission, we check if
+                # our u and v values match, and that is equal to val.
+                # if it is, then we were the one who started the transmission,
+                # and is being asked to retransmit by someone else in
+                # the network, thereby ensuring a cycle
+                if self.u.value == self.v.value and self.u.value == val:
+                    print("DEADLOCK")
+                    sys.exit(1)
+                else:
+                    self.u.value = val
+            else:
+                # we are starting the transmission
+                self.u.value = max(self.u.value, val) + 1
+                self.v.value = self.u.value
+                val = self.u.value
+        # transmit to all nodes which are blocked by current node
+        for node in self.blocking:
+            node.transmit(val)
 
 
 def main():
